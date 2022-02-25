e1ce4f01002c1c526ad90ccaa51590957d51f25a
mnishimori <mnishimori@yahoo.com.br>
2022-02-25 20:06:14 -0300

    Refactoring do codigo, criacao de modulo compartilhado

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [CommonModule, ReactiveFormsModule, MensagemModule],
+  exports: [ReactiveFormsModule, MensagemModule],
+})
+export class SharedModule {}
